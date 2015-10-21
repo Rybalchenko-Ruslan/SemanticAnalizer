c9fdfeddb375dd3f64becb1abfbffe9a8e1ec714
Ruslan <gluk.neuron@gmail.com>
2015-10-21 16:01:35 +0300

    First commit
    Create github

---
+++ SemanticsAnalizer/SemanticAnalizer.ts
@@ -0,0 +1,135 @@
+﻿module SemanticAnalizer {
+    export interface Type {
+        ID: string;
+        Name: string;
+        Icon: string;
+    }
+    export class SemanticWords {
+        [func: string]: string[];
+    }
+    export interface RootSemantic {
+        roots: string[];
+        excludes: string[];
+    }
+    class CompileRootSemantic {
+        private roots: RegExp[];
+        private excludes: RegExp[];
+
+        static compile(rootSemantic: RootSemantic): CompileRootSemantic {
+            var c = new CompileRootSemantic();
+            c.roots = new Array<RegExp>(rootSemantic.roots.length);
+            c.excludes = new Array<RegExp>(rootSemantic.excludes.length);
+            for (var i in rootSemantic.roots)
+                c.roots[i] = new RegExp(rootSemantic.roots[i],"gi");
+            for (var i in rootSemantic.excludes)
+                c.excludes[i] = new RegExp(rootSemantic.excludes[i], "gi");
+            return c;
+        }
+
+        match(word: string): boolean {
+            var ret = false;
+            for (var i in this.roots) {
+                if (this.roots[i].test(word)) {
+                    for (var j in this.excludes) {
+                        if (this.excludes[j].test(word))
+                            return false;
+                    }
+                    ret = ret || true;
+                }
+            }
+            return ret;
+        }
+    }
+    export interface SemanticGroup {
+        rootGroups: RootSemantic[];
+    }
+    class CompileSemanticGroup {
+        private rootGroups: CompileRootSemantic[];
+
+        static compile(semantic: SemanticGroup): CompileSemanticGroup {
+            var s = new CompileSemanticGroup();
+            s.rootGroups = new Array<CompileRootSemantic>(semantic.rootGroups.length);
+
+            for (var i in semantic.rootGroups) {
+                s.rootGroups[i] = CompileRootSemantic.compile(semantic.rootGroups[i]);
+            }
+            return s;
+        }
+
+        match(word: string): boolean {
+            for (var index in this.rootGroups)
+                if (this.rootGroups[index].match(word))
+                    return true;
+            return false;
+        }
+
+        withoutQuestion(questions: string[]): CompileSemanticGroup {
+            var s = new CompileSemanticGroup();
+            s.rootGroups = new Array<CompileRootSemantic>();
+            for (var i in this.rootGroups) {
+                if (questions.every((e) => !this.rootGroups[i].match(e)))
+                    s.rootGroups.push(this.rootGroups[i]);
+            }
+            return s;
+        }
+    }
+    export interface Semantic {
+        [func: string]: SemanticGroup;
+    }
+    class CompileSemantic {
+        [func: string]: CompileSemanticGroup;
+
+        static compile(semantic: Semantic): CompileSemantic {
+            var s: CompileSemantic = {};
+            for (var key in semantic) {
+                s[key] = CompileSemanticGroup.compile(semantic[key]);
+            }
+            return s;
+        }
+    }
+
+    export interface Data {
+        question: string;
+        answer: string;
+    }
+
+    export class SemanticAnalizer {
+
+        private semantic: Semantic;
+        private separators: RegExp;
+        private compileSemantics: CompileSemantic;
+
+        constructor(groups: Semantic) {
+            this.semantic = groups;
+            this.separators = /\s+|\?|,|\.|<([^<^>]*)>/gm;
+        }
+
+        compile(): SemanticAnalizer {
+            this.compileSemantics = CompileSemantic.compile(this.semantic);
+            return this;
+        }
+
+        analize(data: Data): SemanticWords {
+            if (!this.compileSemantics)
+                this.compile();
+
+            var words: SemanticWords = {};
+
+            var q = data.question.split(this.separators);
+            var a = data.answer.split(this.separators);
+            for (var key in this.compileSemantics) {
+                var targetSemantic = this.compileSemantics[key].withoutQuestion(q);
+                for (var i in a) { 
+                    if (!a[i]) continue;
+                    if (targetSemantic.match(a[i])) {
+                        if (!words[key])
+                            words[key] = [a[i]];
+                        else
+                            words[key].push(a[i]);
+                    }
+                }
+            }
+            return words;
+        }
+    }
+}
---
+++ SemanticsAnalizer/app.ts
@@ -0,0 +1,135 @@
+﻿/// <reference path="jquery.d.ts" />
+/// <reference path="semanticanalizer.ts" />
+interface Question {
+    question: string;
+    type: string;
+}
+
+interface Data {
+    types: SemanticAnalizer.Type[];
+    semantic: SemanticAnalizer.Semantic;
+    preset: Question[];
+}
+
+class App {
+    private analizer: SemanticAnalizer.SemanticAnalizer;
+    private types: SemanticAnalizer.Type[];
+
+    loadAnalizer(result: SemanticAnalizer.Semantic) {
+        this.analizer = new SemanticAnalizer.SemanticAnalizer(result).compile();
+    }
+
+    loadTypes(result: SemanticAnalizer.Type[]) {
+        this.types = result;
+    }
+
+    loadPreset(result: Question[]) {
+        if (!result) {
+            this.insert();
+            return;
+        }
+        for (var i in result) this.insert(result[i]);
+    }
+
+    private insert(question: Question = undefined) {
+        var $root = $("<div>", { "class": "semantic", "id": "num" + $(".semantic").length });
+        $root.append($("<a href='#'>X</a>").addClass("close"));
+        $root.append($("<div>", { "html": "Question" }))
+            .append($("<div>", { "contenteditable": "true", "class": "edit question", "html": (!question ? "" : question.question) }))
+            .append($("<div>", { "html": "Answer" }))
+            .append($("<div>", { "contenteditable": "true", "class": "edit answer" }))
+            .append($("<div>", { "html": "Result" }))
+            .append($("<div>", { "class": "result" }));
+        var $select = $("<select>");
+        for (var i in this.types) {
+            var option = $("<option>").val(this.types[i].ID).html(this.types[i].Name);
+            $select.append(option);
+            if (question && question.type == this.types[i].ID)
+                option.attr("selected", "selected");
+        }
+        $root.append($("<div>").html("Type")).append($select);
+        
+        var app = this;
+        $("a.close").click((e) => app.remove(e));
+        $("#content").append($root);
+    }
+
+    private remove(e: JQueryEventObject) {
+        e.stopPropagation();
+        $(e.target).parent().empty();
+        $(".semantic:empty").remove();
+    }
+
+    private printSemantic(text: string, semanticWords: SemanticAnalizer.SemanticWords): string {
+        for (var key in semanticWords) {
+            var words = semanticWords[key];
+            for (var i in words) {
+                if (!new RegExp("(<b.*>)(" + words[i] + ")(<\/b>)", "gi").test(text))
+                    text = text.replace(new RegExp(words[i], "gi"), "<b class='" + key + "'>" + words[i] + "</b>");
+            }
+        }
+        return text;
+    }
+
+    private analize() {
+        var app = this;
+        $(".semantic").each((i,a) => {
+            var quest = $(a).children(".question").first();
+            var ans = $(a).children(".answer").first();
+            if (!ans.text()) return;
+            var result = app.analizer.analize({
+                answer: ans.text(),
+                question: quest.text()
+            });
+            if (!result) return;
+            ans.html(app.printSemantic(ans.html(), result));
+            app.titleReassign();
+            $(a).children(".result").first().html(app.count(result));
+        });
+    }
+
+    private count(sem: SemanticAnalizer.SemanticWords): string {
+        var result = "";
+        for (var i in sem)
+            for (var j in this.types)
+                if (this.types[j].ID == i)
+                    result += this.types[j].Icon + ":" + sem[i].length+" ";
+        return result;
+    }
+
+    private titleReassign() {
+        for (var i in this.types) {
+            $("b." + this.types[i].ID).attr("title", this.types[i].Name);
+        }
+    }
+
+    initFromWeb() {
+        var app = this;
+        $.getJSON("semantic.json", (result: SemanticAnalizer.Semantic) => {
+            app.loadAnalizer(result);
+        });
+        $.getJSON("type.json", (result: SemanticAnalizer.Type[]) => {
+            app.loadTypes(result);
+            $.getJSON("preset.json", (result: Question[]) => {
+                app.loadPreset(result);
+            });
+        });
+    }
+
+    initLocal(data: Data) {
+        var app = this;
+        app.loadAnalizer(data.semantic);
+        app.loadTypes(data.types);
+        app.loadPreset(data.preset);
+    }
+
+    start(start: string) {
+        var app = this;
+        $(start).click(function () { app.analize(); });
+    }
+
+    addNew(addNew: string) {
+        var app = this;
+        $(addNew).click(function () { app.insert(); });
+    }
+}
